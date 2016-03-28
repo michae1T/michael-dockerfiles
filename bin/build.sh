f77d657d26b77b6240e5f7eacd773cfa859d591c
Michael T <michae1T@live.ca>
2016-03-28 19:48:48 -0400

    various tweaks
    - add healthcheck endpoint to openvpn
    - fix option overide for docker build
    - add common base image for net images
       to reduce overall image sizes

--- bin/build.sh
+++ bin/build.sh
@@ -2,7 +2,7 @@
 
 . `dirname $0`/_shared.sh
 
-echo "docker build -t $BUILD_FLAGS \"$IMAGE_URI\" $CONTEXT_PATH"
+echo "docker build $BUILD_FLAGS -t \"$IMAGE_URI\" $CONTEXT_PATH"
 
 docker build $BUILD_FLAGS -t "$IMAGE_URI" $CONTEXT_PATH
 
